dc4c2487c42bd4a54606e6c64985d7246c853f1b
John Patrick Madrigal <jp.madrigal07@gmail.com>
2023-03-31 20:39:06 +0800

    feat: Added lighting page

---
+++ app/lighting/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+import Content from "@/components/pages/lighting/Content";
+
+export default function Lighting() {
+  return (
+    <>
+        <Header title="LIGHTING" />
+        <Content/>
+    </>
+  )
+}
